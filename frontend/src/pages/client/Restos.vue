bfe0931c118822c04d18e21e88af6d78e918b631
Kylianmgt <kylian.migot@viacesi.fr>
2022-06-02 16:46:54 +0200

    Create Restaurant Page, Restaurant Card and start restos dataset

--- frontend/src/pages/client/Restos.vue
+++ frontend/src/pages/client/Restos.vue
@@ -2,15 +2,19 @@
 <base-layout
     :show-menu-button="false"
   >
+  <ion-page>
     <ion-text>
       <h1>Les Restos autour de chez vous</h1>
     </ion-text>
+    <RestaurantCard/>
+  </ion-page>
     </base-layout>
 </template>
 
 <script>
 
 import {
+  IonPage,
   IonInput,
   IonRow,
   IonCol,
@@ -21,15 +25,34 @@ import {
 } from '@ionic/vue';
 import { useRouter } from 'vue-router';
 
+import RestaurantCard from '@/components/molecules/restos/RestaurantCard';
+
 
 
 export default {
     name:'Restos',
     components:{
+        RestaurantCard,
         IonIcon,
+        IonText,
+        IonPage
+
     },
     setup() {
-    
+        const router = useRouter();
+        const data = [
+          {
+            name: "MyTacos",
+            address: "1 rue de la paix",
+            description: "Tacos de la meilleure qualité",
+            image: "https://img1.freepng.fr/20180717/yfu/kisspng-el-risitas-issou-laughter-jeuxvideo-com-sticker-issou-hd-5b4d7d6b1b77c2.1011126415318050351125.jpg",
+            rating: 4.5,
+          }
+        ]
+
+        return {
+            router
+        }
   },
     methods: {
     },
