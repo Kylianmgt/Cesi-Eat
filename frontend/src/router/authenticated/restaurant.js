211a0002206b493df249180a76ffb5493ceaaf1a
Mathéo Berger <matheo.berger@viacesi.fr>
2022-06-17 10:36:37 +0200

    fixed end of merge conflit

--- frontend/src/router/authenticated/restaurant.js
+++ frontend/src/router/authenticated/restaurant.js
@@ -2,18 +2,11 @@ import UserTypes from '../../enums/UserTypes';
 
 export default [
     {
-<<<<<<< HEAD
-        path: '/restaurant/home',
-        name: 'restaurant-home',
-        component: () => import('../../pages/restaurant/Home.vue'),
-        meta: { userType: UserTypes.RESTAURANT },
-=======
         name: 'MyRestaurant',
         role: ["restaurant"],
         icon: 'restaurant',
         placeholder: "MyRestaurant",
         path: '/restaurant',
         component: () => import('../../pages/restaurant/Restaurant.vue'),
->>>>>>> master
     },
 ];
