f8de92a00888d621894520a0731cddc70aec1bb3
Cnsbmkpsr <44788831+Cnsbmkpsr@users.noreply.github.com>
2022-06-20 15:05:11 +0200

    Create MenuEdit.vue

---
+++ frontend/src/pages/restaurant/MenuEdit.vue
@@ -0,0 +1,36 @@
+<template>
+  <base-layout :show-menu-button="false" pageTitle="Mon restaurant">
+
+   </base-layout>
+</template>
+
+
+<script>
+import {
+  IonItem,
+  IonIcon,
+} from "@ionic/vue";
+import { useRouter } from "vue-router";
+
+export default {
+  name: "Restaurant",
+  components: {
+    IonIcon,
+    IonItem
+  },
+
+  setup() {
+    const router = useRouter();
+    return {
+      router,
+      data,
+    };
+  },
+  methods: {},
+
+};
+</script>
+
+<style scoped>
+
+</style>
