18f7ec9cedc96178deab54211bcb0f53d597a832
Cnsbmkpsr <44788831+Cnsbmkpsr@users.noreply.github.com>
2022-06-20 15:37:10 +0200

    Fixing 5 Eslint errors

--- frontend/src/pages/restaurant/MenuEdit.vue
+++ frontend/src/pages/restaurant/MenuEdit.vue
@@ -1,36 +1,45 @@
 <template>
-  <base-layout :show-menu-button="false" pageTitle="Mon restaurant">
-
+  <base-layout :show-menu-button="false" pageTitle="Editer un Menu">
+    <ion-page>
+        <ion-content>
+            <div>Oui</div>
+        </ion-content>
+    </ion-page>
    </base-layout>
 </template>
 
 
 <script>
 import {
+  IonPage,
+  IonInput,
   IonItem,
+  IonTitle,
   IonIcon,
+  IonText,
+  IonToolbar 
 } from "@ionic/vue";
 import { useRouter } from "vue-router";
 
 export default {
-  name: "Restaurant",
+  name: "MenuEdit",
   components: {
     IonIcon,
+    IonInput,
+    IonText,
+    IonPage,
+    IonTitle,
+    IonToolbar,
     IonItem
   },
 
   setup() {
     const router = useRouter();
     return {
-      router,
-      data,
+      router
     };
   },
   methods: {},
 
 };
 </script>
-
-<style scoped>
-
-</style>
