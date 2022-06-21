00119f24503b6b7f35b625f0b4a9b6a0beb76ef5
Cnsbmkpsr <44788831+Cnsbmkpsr@users.noreply.github.com>
2022-06-21 13:14:07 +0200

    Simple Form Display Restaurant Informations

--- frontend/src/pages/restaurant/RestaurantEdit.vue
+++ frontend/src/pages/restaurant/RestaurantEdit.vue
@@ -4,29 +4,39 @@
         <ion-content>
             <!-- Mains informations about restaurant -->
             <IonTitle size="large" color="primary">Mes Informations</IonTitle>
-            <div class="flex p-8">
-              <ion-grid>
-                <ion-col>
-                  Nom du restaurant: {{ restaurant.name }}
-                </ion-col>
-                  <br>
-                <ion-col>
-                  Description: {{ restaurant.description }}
-                  <br>
-                </ion-col>
-                <ion-col>
-                  Ville: {{ restaurant.city }}
-                  <br>
-                </ion-col>
-                <ion-col>
-                  Address: {{ restaurant.address }}
-                  <br>
-                </ion-col>
-                <ion-col>
-                  Note des utilisateurs: {{ restaurant.rating }}
-                </ion-col>
-              </ion-grid>
-              <ion-button @click="() => router.push({ name: 'MyRestaurant' })">Enregistrer les modifications</ion-button>
+            <div class="flex flex-col p-8 ">
+
+            <ion-item>
+                <ion-label position="floating">Nom du restaurant </ion-label>
+                <ion-input type="text" v-model="restaurant.name" />
+            </ion-item>
+
+            <ion-item>
+                <ion-label position="floating">Description du restaurant </ion-label>
+                <ion-input type="text" v-model="restaurant.description" />
+            </ion-item>
+
+            <ion-item>
+                <ion-label position="floating">Addresse du restaurant </ion-label>
+                <ion-input type="text" v-model="restaurant.address" />
+            </ion-item>
+
+            <ion-item>
+                <ion-label position="floating">Note du restaurant </ion-label>
+                <ion-input type="text" v-model="restaurant.rating" />
+            </ion-item>
+
+            <ion-item>
+                <ion-label position="floating">Note du restaurant </ion-label>
+                <ion-input type="text" v-model="restaurant.distance" />
+            </ion-item>
+
+            <ion-item>
+                <ion-label position="floating">Note du restaurant </ion-label>
+                <ion-input type="text" v-model="restaurant.city" />
+            </ion-item>
+
+              <ion-button color="secondary" @click="() => router.push({ name: 'MyRestaurant' })">Enregistrer les modifications</ion-button>
             </div>
         </ion-content>
     </ion-page>
@@ -47,7 +57,9 @@ import {
     IonButton,
     IonCard,
     IonCardContent,
-    IonContent
+    IonContent,
+    IonLabel,
+
 } from "@ionic/vue";
 import { useRouter, useRoute } from "vue-router";
 
@@ -65,7 +77,8 @@ export default {
     IonButton,
     IonCard,
     IonCardContent,
-    IonContent
+    IonContent,
+    IonLabel,
   },
 
   props: route => ({
